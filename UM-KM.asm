9b077e9744189649997fd779d92ca5a911a27c2d
Eremetic <146580877+Eremetic@users.noreply.github.com>
2024-07-19 23:02:56 -0400

    Update and rename Source to UM-KM.asm

--- UM-KM.asm
+++ UM-KM.asm
@@ -1,45 +1,6 @@
 .code
 
 
-;EXTERN_C const uintmax_t __stdcall SdbmHash(const char* str);
-SdbmHash proc
-
-	push rbx ; Preserve non volatile registers
-	push rsi
-	push rdi
-	push rbp
-	
-	push rcx ; save string
-	xor rax, rax ; clear register for hash
-	xor rbx, rbx ; clear register for index
-
-	mov rsi, rcx ; Set the string
-	Hash:
-	lodsb ; Load the char from the string
-	test al, al ; Check if the string is over
-	jz HashEnd ; If it is, end the hash
-
-	shl rax, 06h
-	add rax, rbx ; add the hash
-	shl rax, 10h
-	sub rax, rbx ; subtract the hash
-
-	jmp Hash
-
-HashEnd:
-	pop rcx ; restore string
-	
-	pop rbp ; Restore non volatile registers
-	pop rdi
-	pop rsi
-	pop rbx
-
-
-	ret
-SdbmHash endp
-
-
-
 ;EXTERN_C void* __stdcall FunctionExport(__in const uintmax_t functionHash, __in void* dllBase);
 FunctionExport proc
 
